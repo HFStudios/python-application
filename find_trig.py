de0d28798132833668b26a809924f57f0d40b23a
HF <1hassanfarooq@gmail.com>
2017-10-27 16:48:00 -0700

    Get good
    Better readability

--- find_trig.py
+++ find_trig.py
@@ -1,9 +1,10 @@
 import math
-n=1
+
+
 def tri(n):
-    x=1
-    if (length2+length3 > hy and hy+length2 > length3 and hy+length3 > length2):
-        if ((hy**2) == (length2**2)+(length3**2)):
+    x = 1
+    if (length2 + length3 > hy and hy + length2 > length3 and hy + length3 > length2):
+        if ((hy ** 2) == (length2 ** 2) + (length3 ** 2)):
             print("Right Triangle")
         elif (hy == length2 == length3):
             print("Equalatoral Trangle")
@@ -15,34 +16,40 @@ def tri(n):
             print("Not a Triangle")
     else:
          print("Not a Triangle")
-        
-fun = input("find the triangle with length or Points? ")
+
+
+
+
+
+fun = input("Find the triangle with length or Points? ")
 if (fun == "length"):
     hy = int(input("what is hypotenuse? "))
     length2 = int(input("what is length2? "))
     length3 = int(input("what is length3? "))
-    tri(n)
+    tri(1)
 
 elif(fun == "points"):
-    hy=0
+    hy = 0
     x1 = int(input("what is x1? "))
     y1 = int(input("what is y1? "))
     x2 = int(input("what is x2? "))
     y2 = int(input("what is y2? "))
     x3 = int(input("what is x3? "))
     y3 = int(input("what is y3? "))
-    dis1 = math.sqrt(((x2-x1)**2)+((y2-y1)**2))
-    dis2 = math.sqrt(((x3-x2)**2)+((y3-y2)**2))
-    dis3 = math.sqrt(((x3-x1)**2)+((y3-y1)**2))
-    if (dis1+dis2 > dis3 and dis2+dis3 > dis1 and dis3+dis1 > dis2):
+
+    dis1 = math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
+    dis2 = math.sqrt(((x3 - x2) ** 2) + ((y3 - y2) ** 2))
+    dis3 = math.sqrt(((x3 - x1) ** 2) + ((y3 - y1) ** 2))
+
+    if (dis1 + dis2 > dis3 and dis2 + dis3 > dis1 and dis3 + dis1 > dis2):
         if (dis1 > dis2 and dis1 > dis3):
             hy = dis1
             if (x1 == x2 == x3 or y1 == y2 == y3):
                 print("Not a Triangle")
-            if ((hy**2) == (dis2**2)+(dis3**2)):
+            if ((hy ** 2) == (dis2 ** 2) + (dis3 ** 2)):
                 print("Right Triangle")
             elif(hy == dis2 == dis3):
-                print("Equalatoral Triangle")
+                print("Equilateral Triangle")
             elif(hy == dis2 or hy == dis3 or dis2 == dis3):
                 print("Isosceles Triangle")
             elif(hy != dis2 and dis2 != dis3 and hy != dis3):
@@ -51,10 +58,10 @@ elif(fun == "points"):
             hy = dis2
             if (x1 == x2 == x3 or y1 == y2 == y3):
                 print("Not a Triangle")
-            if ((hy**2) == (dis1**2)+(dis3**2)):
+            if ((hy ** 2) == (dis1 ** 2) + (dis3 ** 2)):
                 print("Right Triangle")
             elif(hy == dis1 == dis3):
-                print("Equalatoral Triangle")
+                print("Equilateral Triangle")
             elif(hy == dis1 or hy == dis3 or dis1 == dis3):
                 print("Isosceles Triangle")
             elif(hy != dis1 and dis1 != dis3 and hy != dis3):
@@ -63,7 +70,7 @@ elif(fun == "points"):
             hy = dis3
             if (x1 == x2 == x3 or y1 == y2 == y3):
                 print("Not a Triangle")
-            if ((hy**2) == (dis1**2)+(dis2**2)):
+            if ((hy ** 2) == (dis1 ** 2) + (dis2 ** 2)):
                 print("Right Triangle")
             elif(hy == dis1 == dis2):
                 print("Equalatoral Triangle")
