9ce6b5ea397466c7059640c43838f37d34a7e164
HF <1hassanfarooq@gmail.com>
2017-10-27 18:29:55 -0700

    GUI Changes
    GUI works, but doesnt send answer

--- basicGUI.py
+++ basicGUI.py
@@ -2,24 +2,22 @@ import sys
 from tkinter import *
 
 
+#Function called when button pressed
 def enterValues():
     print("VALUES: " + str(getHypotenuse.get()) + " " + str(getSideX.get()) + " " + str(getSideY.get()))
     givenLengths(getHypotenuse.get(), getSideX.get(), getSideY.get())
 
 
 
-
-
-
+#Setting up GUI and variables for input
 mGUI = Tk()
 getHypotenuse = IntVar()
 getSideX = IntVar()
 getSideY = IntVar()
-
-
 mGUI.geometry("600x600")
 mGUI.title("Triangle Calculator")
 
+#Labels/input fields for GUI
 mLabel = Label(mGUI, text = "Hypotenuse").pack()
 mEntry = Entry(mGUI, textvariable = getHypotenuse).pack()
 
@@ -30,8 +28,9 @@ mLabel = Label(mGUI, text = "SideY").pack()
 mEntry = Entry(mGUI, textvariable = getSideY).pack()
 
 
-
-
+#Button in GUI
 mButton = Button(mGUI, text = "Submit Values", command = enterValues, fg = "white", bg = "black").pack()
 
+
+#Run GUI
 mGUI.mainloop()
